6256ed70b6c117b3077b79d792480328e793cb60
gezhiwei <gezhiwei8899@gmail.com>
2018-03-14 16:17:32 +0800

    container

--- python/jdd_docker_deploy.py
+++ python/jdd_docker_deploy.py
@@ -10,11 +10,15 @@ port = '2376'
 print 'start to deploy docker image to the server'
 container = get_container_info_by_container_name(ip, port, DOCKER_CONTAINER_NAME)
 if container == 'connect_error':
-    global exception
     exception = 'please check the target env,Network connect error,ip: ' + ip
+    raise exception
 elif (container is None or container == '!exsit'):
     print 'container is not !exist'
-    pull_docker_image(ip, port, DOCKER_IMAGE_NAME, DOCKER_IMAGE_TAG)
+    status_code = pull_docker_image(ip, port, DOCKER_IMAGE_NAME, DOCKER_IMAGE_TAG)
+    if status_code == '-1':
+        exception = 'pull images failed'
+        raise exception
+
     create_container(ip, port, DOCKER_CONTAINER_NAME, CURRENT_DOCKER_WITH_TAG, paramter_port=PARAMTER_PORT,
                      paramter_volume=PARAMTER_VOLUME, paramter_Entrypoint=PARAMTER_ENTRYPOINT)
     start_container(ip, port, DOCKER_CONTAINER_NAME)
